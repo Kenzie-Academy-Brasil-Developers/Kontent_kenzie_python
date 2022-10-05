ba688e01792eb46b664b151d26b480ae7f896819
Bruno Gomes <gomesbruno030@outlook.com>
2022-10-05 15:11:22 -0300

    add p1 sem o metodo

---
+++ content/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# musicians/models.py
+
+class Content(models.Model):
+    title = models.CharField(max_length=50)
+    module = models.TextField()
+    students = models.IntegerField()
+    description = models.TextField()
+    is_active = models.BooleanField(default = False)
